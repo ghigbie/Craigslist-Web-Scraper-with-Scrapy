1ad6c981ae68bc2603b55095f02a155f05946a10
ghigbie <gehigbie@gmail.com>
2019-04-14 10:25:31 -0600

    Add print statement

--- craigslistJobs/spiders/jobs.py
+++ craigslistJobs/spiders/jobs.py
@@ -5,7 +5,9 @@ import scrapy
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
     allowed_domains = ['newyork.craigslist.org']
-    start_urls = ['http://newyork.craigslist.org/']
+    start_urls = ['http://newyork.craigslist.org/search/egr']
 
     def parse(self, response):
-        pass
+        listings = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+        for listing in listings:
+            print listing
